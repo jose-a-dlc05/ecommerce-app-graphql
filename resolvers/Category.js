935b44620eec14cabfa5fd1ef191d1f5c039ea91
Jose De La Cruz <jose.delcruz0490@icloud.com>
2022-04-25 01:06:41 -0400

    Added average rating filter

--- resolvers/Category.js
+++ resolvers/Category.js
@@ -1,15 +1,32 @@
 exports.Category = {
-	products: ({ id: categoryId }, { filter }, { products }) => {
+	products: ({ id: categoryId }, { filter }, { products, reviews }) => {
 		const categoryProducts = products.filter(
 			(product) => product.categoryId === categoryId
 		);
 		let filteredCategoryProducts = categoryProducts;
 
 		if (filter) {
-			if (filter.onSale === true) {
+			const { onSale, avgRating } = filter;
+			if (onSale) {
 				filteredCategoryProducts = filteredCategoryProducts.filter(
 					(product) => {
 						return product.onSale;
+					}
+				);
+			}
+			if ([1, 2, 3, 4, 5].includes(avgRating)) {
+				filteredCategoryProducts = filteredCategoryProducts.filter(
+					(product) => {
+						let sumRating = 0;
+						let numOfReviews = 0;
+						reviews.forEach((review) => {
+							if (review.productId === product.id) {
+								sumRating += review.rating;
+								numOfReviews++;
+							}
+						});
+						const avgProductRating = sumRating / numOfReviews;
+						return avgProductRating >= avgRating;
 					}
 				);
 			}
--- resolvers/Query.js
+++ resolvers/Query.js
@@ -2,13 +2,28 @@ exports.Query = {
 	hello: () => {
 		return ['World!!!'];
 	},
-	products: (parent, { filter }, { products }) => {
+	products: (parent, { filter }, { products, reviews }) => {
 		let filteredProducts = products;
 
 		if (filter) {
-			if (filter.onSale === true) {
+			const { onSale, avgRating } = filter;
+			if (onSale) {
 				filteredProducts = filteredProducts.filter((product) => {
 					return product.onSale;
+				});
+			}
+			if ([1, 2, 3, 4, 5].includes(avgRating)) {
+				filteredProducts = filteredProducts.filter((product) => {
+					let sumRating = 0;
+					let numOfReviews = 0;
+					reviews.forEach((review) => {
+						if (review.productId === product.id) {
+							sumRating += review.rating;
+							numOfReviews++;
+						}
+					});
+					const avgProductRating = sumRating / numOfReviews;
+					return avgProductRating >= avgRating;
 				});
 			}
 		}
